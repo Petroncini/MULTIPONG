1e385a39d43790a584e67d886f2a4ce9a3d43f1e
Petroncini <caiopetroncini@gmail.com>
2025-11-17 08:35:13 -0300

    semaphore graphic update

--- pong.cpp
+++ pong.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <semaphore>
 #include <sys/select.h>
 #include <termios.h>
 #include <thread>
@@ -18,9 +19,11 @@ struct GAMESTATE {
   int p2score;
   float bx, by;
   float bvx, bvy;
+  vector<vector<char> > grid;
 };
 
 GAMESTATE gameState;
+binary_semaphore updateGraphics(1);
 
 struct termios oldt, newt;
 
@@ -51,32 +54,15 @@ char getch() {
 void graphicsThread() {
 
   while (true) {
-    vector<vector<char> > grid(HEIGHT, vector<char>(WIDTH, '.'));
+    updateGraphics.acquire();
     std::cout << "\033[2J\033[1;1H";
 
     for (int i = 0; i < HEIGHT; i++) {
-      grid[i][0] = '.';
-      grid[i][WIDTH / 2] = '|';
-      grid[i][WIDTH - 1] = '.';
-    }
-
-    grid[max(gameState.p1y - 1, 0)][0] = '#';
-    grid[gameState.p1y][0] = '#';
-    grid[min(gameState.p1y + 1, HEIGHT - 1)][0] = '#';
-
-    grid[max(gameState.p2y - 1, 0)][WIDTH - 1] = '#';
-    grid[gameState.p2y][WIDTH - 1] = '#';
-    grid[min(gameState.p2y + 1, HEIGHT - 1)][WIDTH - 1] = '#';
-
-    grid[int(gameState.by)][int(gameState.bx)] = 'O';
-
-    for (int i = 0; i < HEIGHT; i++) {
       for (int j = 0; j < WIDTH; j++) {
-        cout << grid[i][j];
+        cout << gameState.grid[i][j];
       }
       cout << endl;
     }
-    usleep(5000);
   }
 }
 
@@ -85,14 +71,38 @@ void playerThread() {
     if (kbhit()) {
       char c = getch();
       if (c == 'w') {
+        int oy = gameState.p1y;
         gameState.p1y = max(gameState.p1y - 1, 1);
+        if (gameState.p1y != oy) {
+          gameState.grid[oy + 1][0] = '.';
+          gameState.grid[gameState.p1y - 1][0] = '#';
+          updateGraphics.release();
+        }
       } else if (c == 's') {
-        gameState.p1y = min(gameState.p1y + 1, HEIGHT - 1);
+        int oy = gameState.p1y;
+        gameState.p1y = min(gameState.p1y + 1, HEIGHT - 2);
+        if (gameState.p1y != oy) {
+          gameState.grid[oy - 1][0] = '.';
+          gameState.grid[gameState.p1y + 1][0] = '#';
+          updateGraphics.release();
+        }
       }
       if (c == 'i') {
+        int oy = gameState.p2y;
         gameState.p2y = max(gameState.p2y - 1, 1);
+        if (gameState.p2y != oy) {
+          gameState.grid[oy + 1][WIDTH - 1] = '.';
+          gameState.grid[gameState.p2y - 1][WIDTH - 1] = '#';
+          updateGraphics.release();
+        }
       } else if (c == 'k') {
-        gameState.p2y = min(gameState.p2y + 1, HEIGHT - 1);
+        int oy = gameState.p2y;
+        gameState.p2y = min(gameState.p2y + 1, HEIGHT - 2);
+        if (gameState.p2y != oy) {
+          gameState.grid[oy - 1][WIDTH - 1] = '.';
+          gameState.grid[gameState.p2y + 1][WIDTH - 1] = '#';
+          updateGraphics.release();
+        }
       }
     }
   }
@@ -115,20 +125,60 @@ void ballThread() {
     nx = max(0.0f, min(float(WIDTH - 1), nx));
     ny = max(0.0f, min(float(HEIGHT - 1), ny));
 
+    int ix = int(ox);
+    int iy = int(oy);
+
+    if (ix == 0 && (iy == gameState.p1y - 1 || iy == gameState.p1y ||
+                    iy == gameState.p1y + 1)) {
+      gameState.grid[iy][ix] = '#';
+    } else if (ix == WIDTH - 1 &&
+               (iy == gameState.p2y - 1 || iy == gameState.p2y ||
+                iy == gameState.p2y + 1)) {
+      gameState.grid[iy][ix] = '#';
+    } else if (ix == WIDTH / 2) {
+      gameState.grid[iy][ix] = '|';
+    } else {
+      gameState.grid[iy][ix] = '.';
+    }
+
     gameState.bx = nx;
     gameState.by = ny;
+    gameState.grid[ny][nx] = 'O';
+    updateGraphics.release();
 
     usleep(10000);
   }
 }
 
-int main(void) {
+void initGameState(void) {
   gameState.bx = 10;
   gameState.by = 10;
   gameState.bvx = 0.2;
-  gameState.bvy = -0.2;
+  gameState.bvy = 0.2;
   gameState.p1y = HEIGHT / 2;
   gameState.p2y = HEIGHT / 2;
+
+  gameState.grid.resize(HEIGHT, vector<char>(WIDTH, '.'));
+
+  for (int i = 0; i < HEIGHT; i++) {
+    gameState.grid[i][0] = '.';
+    gameState.grid[i][WIDTH / 2] = '|';
+    gameState.grid[i][WIDTH - 1] = '.';
+  }
+
+  gameState.grid[max(gameState.p1y - 1, 0)][0] = '#';
+  gameState.grid[gameState.p1y][0] = '#';
+  gameState.grid[min(gameState.p1y + 1, HEIGHT - 1)][0] = '#';
+
+  gameState.grid[max(gameState.p2y - 1, 0)][WIDTH - 1] = '#';
+  gameState.grid[gameState.p2y][WIDTH - 1] = '#';
+  gameState.grid[min(gameState.p2y + 1, HEIGHT - 1)][WIDTH - 1] = '#';
+
+  gameState.grid[int(gameState.by)][int(gameState.bx)] = 'O';
+}
+
+int main(void) {
+  initGameState();
 
   enableRawMode();
 
